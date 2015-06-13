14ce5e68ab14f97346b48d6680e1278ab0e2202e
aliyyousuff <aliyyousuf@gmail.com>
2015-06-13 21:13:32 +0600

    Create as3.py

---
+++ Week02/as3.py
@@ -0,0 +1,7 @@
+# This assignment was the part of week 02.
+
+
+hours = raw_input("Enter total hours: ")
+pay_per_hour = raw_input("Enter amount: ")
+total_payment = float(hours) * float(pay_per_hour)
+print total_payment
